a19dbf189306df24512108698e0759ff8ab568fd
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-17 13:23:22 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,7 @@ while i < collection.length
 block_return_values<< yield(collection[i])
 i += 1
 end
-if block_return_values.included?(false)
+if block_return_values.include?(false)
   false
 else 
   true
